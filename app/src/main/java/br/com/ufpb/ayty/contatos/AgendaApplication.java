b1fffbd06ccc397e51ec7904ab51d95cc6279412
Raphael Salviano <raphael_salviano@outlook.com>
2015-12-03 09:10:13 -0300

    Update AgendaApplication.java
    Comentários da Classe Application

--- app/src/main/java/br/com/ufpb/ayty/contatos/AgendaApplication.java
+++ app/src/main/java/br/com/ufpb/ayty/contatos/AgendaApplication.java
@@ -1,5 +1,16 @@
 package br.com.ufpb.ayty.contatos;
 
+/* Classe base para manter o estado e funções da aplicação global.
+ * A classe Application funciona como um Singleton, onde ela é iniciada sempre que a aplicação é aberta
+ * e é destruída sempre que a aplicação é encerrada. É necessário implementar os métodos onCreate e onTerminate
+ * para iniciar ou finalizar as funcionalidades existentes na classe.
+ * A classe Application pode ser recuperada em qualquer parte da sua aplicação, para isso utilize a seguinte
+ * notação: AgendaApplication application = ((AgendaApplication) getApplicationContext())
+ * { na Activity atual e recupere a ApplicationContext por meio do método getApplicationContext() }
+ * A classe Application deverá ser informada no AndroidManifest.xml na TAG <application> com o seguinte atributo
+ * android:name=".AgendaApplication"
+ */
+
 import android.app.Application;
 
 import java.util.ArrayList;
